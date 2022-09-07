d4d7a633777960f1fae932cee4dd442243d442ce
realKimJaeJUN <nagandr1113@gmail.com>
2022-09-07 19:52:03 +0900

    Bookstore 예제 추가

---
+++ java2_Bookstore.sql
@@ -0,0 +1,11 @@
+select * from `book`;
+insert into `book` values(1, '신라는 어떻게 가야를 먹었나', 23000, '신라출판');
+
+select * from `customer`;
+insert into `customer` values(10101, '김유신', '010-8891-1001', '경남 김해시');
+
+select * from `order`;
+insert into `order` values(int, int, int, datetime, int);
+
+select * from `publisher`;
+insert into `publisher` values(varchar, varchar, char);
